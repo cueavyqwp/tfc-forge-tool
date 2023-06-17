1e7c33794a94eed7ef44b5626712951fa34061c4
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-06-18 00:54:52 +0800

    Creat GUI

--- tfc_anvil.py
+++ tfc_anvil.py
@@ -1,6 +1,9 @@
+from tkinter import ttk
 import tkinter as tk
 import json
 import pip
+
+font = ( "Consolas" , 14 , "bold" )
 
 while 1 :
     try :
@@ -21,16 +24,50 @@ def get_name( i : dict ) :
     return lang.str_replace( i["name"] )
 
 forge = {}
+item = {}
 for i in data["forge"] :
     forge[ i["name"] ] = [ get_name(i) , i["value"] ]
-print(forge)
-item = {}
 for i in data["item"] :
     value = 0
     for I in [ i["value"] ] + [ -forge[I][1] for I in i["end"] ] :
         value += I
-    item[ i["name"] ] = [ get_name(i) , value ]
+    item[ get_name(i) ] = value
+forge_nums = { i[1] : i[0] for i in forge.values() }
 
-print(item)
+combobox = ttk.Combobox( root , justify = tk.CENTER )
+combobox["value"] = tuple( item.keys() )
+combobox["state"] = "readonly"
+combobox.current(0)
+combobox.pack( fill = tk.X )
+
+frame_start = ttk.Frame()
+tk.Label( frame_start , text = lang.get("start") ).pack( side = tk.LEFT )
+start_text = ttk.Entry( frame_start , font = font )
+start_text.pack( fill = tk.X )
+def start_text_init() :
+    start_text.delete( "0" , tk.END )
+    start_text.insert( "0" , 0 )
+start_text_init()
+frame_start.pack( fill = tk.X )
+
+button = ttk.Button( root , text = lang.get("output") )
+button.pack( side = tk.BOTTOM )
+
+text = tk.Text( root , font = font )
+text.pack( anchor = tk.CENTER , fill = tk.BOTH )
+def output_text( text = "" ) :
+    text
+
+def output() :
+    num = item[ combobox.get() ]
+    while 1 :
+        try :
+            start = start_text.get()
+            num += int(start)
+            break
+        except :
+            start_text_init()
+    print(num)
+button.config( command = output )
 
 root.mainloop()
