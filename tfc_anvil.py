a4ac9107522115a932f3d3f9ef371f2258d77b1a
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-06-20 10:11:24 +0800

    some changes

--- tfc_anvil.py
+++ tfc_anvil.py
@@ -1,123 +1,102 @@
-from tkinter import scrolledtext , messagebox , filedialog , ttk
-import tkinter as tk
-import json
-import pip
+from tkinter.filedialog import askopenfilename , asksaveasfilename
+from tkinter.ttk import Combobox , Button , Entry
+from tkinter.scrolledtext import ScrolledText
+from tkinter.messagebox import showerror
+from tkinter import Frame , Label , Tk
+from json import dumps , load
+from os.path import exists
+from os import mkdir
+from pip import main
 
 font = ( "Consolas" , 18 , "bold" )
 
 while 1 :
     try :
-        import langful
+        from langful import lang
         break
     except :
-        pip.main( [ "install" , "langful" ] )
+        main( [ "install" , "langful" ] )
+if not exists( ".\\save" ) :
+    mkdir( ".\\save" )
+del exists , mkdir , main
 
-lang = langful.lang( default_lang = "en_us" , file_type = "lang" )
-# debug
-# lang.use_locale="en_us"
-# lang.use_locale="zh_cn"
-win_size = 400 , 700
-x , y = 0 , 0
 is_debug = True
-root = tk.Tk()
+lang = lang( default_lang = "en_us" , file_type = "lang" )
+root = Tk()
 root.title( lang.get("title") )
-root.geometry( f"{win_size[0]}x{win_size[1]}+200+200" )
+root.geometry( f"400x700" )
 root.attributes( "-topmost" , 1 )
 root.attributes( "-transparent" )
 root.update()
 
-forge = [{
-    "name" : "forge.hit_light" ,
-    "value" : -3
-} , {
-    "name" : "forge.hit_medium" ,
-    "value" : -6
-} , {
-    "name" : "forge.hit_hard" ,
-    "value" : -9
-} , {
-    "name" : "forge.draw" ,
-    "value" : -15
-} , {
-    "name" : "forge.bend" ,
-    "value" : 7
-} , {
-    "name" : "forge.punch" ,
-    "value" : 2
-} , {
-    "name" : "forge.shrink" ,
-    "value" : 16
-} , {
-    "name" : "forge.upset" ,
-    "value" : 13
-}]
+forge = {
+    "forge.hit_light" : -3 ,
+    "forge.hit_medium" : -6 ,
+    "forge.hit_hard" : -9 ,
+    "forge.draw" : -15 ,
+    "forge.punch" : 2 ,
+    "forge.bend" : 7 ,
+    "forge.upset" : 13 ,
+    "forge.shrink" : 16
+}
 
-forge_nums = { i["name"] : i["value"] for i in forge }
-forge_name = { lang.get(i) : i for i in forge_nums }
-forge_nums_name = { v : k for k , v in forge_nums.items() }
+def forge_name() : return { lang.get(i) : i for i in forge }
+def forge_nums() : return { v : k for k , v in forge.items() }
 
-info_frame = tk.Frame()
+info_frame = Frame()
 
-output_button = ttk.Button( info_frame , text = lang.get( "output" ) )
-output_button.pack( side = tk.RIGHT , anchor=tk.E , fill = tk.BOTH )
+output_button = Button( info_frame , text = lang.get( "output" ) )
+output_button.pack( side = "right" , anchor="e" , fill = "both" )
 
-save_button = ttk.Button( root , text = lang.get("save") )
-save_button.pack( side = tk.BOTTOM , fill =  tk.X )
+save_button = Button( root , text = lang.get("save") )
+save_button.pack( side = "bottom" , fill =  "x" )
 
-load_button = ttk.Button( root , text = lang.get("load") )
-load_button.pack( side = tk.BOTTOM , fill =  tk.X )
+load_button = Button( root , text = lang.get("load") )
+load_button.pack( side = "bottom" , fill =  "x" )
 
-start_frame = tk.Frame(info_frame)
-tk.Label( start_frame , text = lang.get("start") ).pack( side = tk.LEFT )
-start_text = ttk.Entry( start_frame , font = font )
-start_text.pack( fill = tk.X )
+start_frame = Frame(info_frame)
+Label( start_frame , text = lang.get("start") ).pack( side = "left" )
+start_text = Entry( start_frame , font = font )
+start_text.pack( fill = "x" )
 def start_text_init( num = 0 ) :
-    start_text.delete( "0" , tk.END )
+    start_text.delete( "0" , "end" )
     start_text.insert( "0" , num )
 start_text_init()
-start_frame.pack( fill = tk.X )
+start_frame.pack( fill = "x" )
 
-end_frame = tk.Frame(info_frame)
-tk.Label( end_frame , text = lang.get("end_p") ).pack( side = tk.LEFT )
-end_text = ttk.Entry( end_frame , font = font )
-end_text.pack( fill = tk.X )
+end_frame = Frame(info_frame)
+Label( end_frame , text = lang.get("end_p") ).pack( side = "left" )
+end_text = Entry( end_frame , font = font )
+end_text.pack( fill = "x" )
 def end_text_init( num = 0 ) :
-    end_text.delete( "0" , tk.END )
+    end_text.delete( "0" , "end" )
     end_text.insert( "0" , num )
 end_text_init()
-end_frame.pack( fill = tk.X )
+end_frame.pack( fill = "x" )
 
 end_combobox = []
-tk.Label( info_frame , text = lang.get( "end" ) ).pack( side = "left" )
-for i in [ [tk.N,tk.LEFT] , [tk.NW,tk.RIGHT] , [tk.NE,tk.LEFT] ] :
-    end_combobox.append( ttk.Combobox( info_frame ) )
-    end_combobox[-1]["value"] = tuple( forge_name.keys() )
+Label( info_frame , text = lang.get( "end" ) ).pack( side = "left" )
+for i in range(3) :
+    end_combobox.append( Combobox( info_frame ) )
+    end_combobox[-1]["value"] = tuple( forge_name().keys() )
     end_combobox[-1]["state"] = "readonly"
     end_combobox[-1].current(0)
-    end_combobox[-1].pack( fill = tk.X )
+    end_combobox[-1].pack( fill = "x" )
 
-info_frame.pack( fill = tk.X )
+info_frame.pack( fill = "x" )
 
-text = scrolledtext.ScrolledText( root , relief = tk.RIDGE , font = font , height = root.winfo_height() )
-text.pack( anchor = tk.CENTER , fill = tk.BOTH )
+text = ScrolledText( root , relief = "ridge" , font = font , height = root.winfo_height() )
+text.pack( anchor = "center" , fill = "both" )
 def output_text( info = "" , end = "\n" , cls = False ) :
-    text.config( state = tk.NORMAL )
+    text.config( state = "normal" )
     if cls :
-        text.delete( "0.0" , tk.END )
+        text.delete( "0.0" , "end" )
         end = ""
-    text.insert( tk.END , str( info ) + end )
-    text.config( state = tk.DISABLED )
+    text.insert( "end" , str( info ) + end )
+    text.config( state = "disabled" )
 output_text("")
 
-def move( event ) :
-    set_x = event.x - x + root.winfo_x()
-    set_y = event.y - y + root.winfo_y()
-    root.geometry(f"{win_size[0]}x{win_size[1]}+{set_x}+{set_y}")
-
-def get_point( event ) :
-    global x , y
-    x = event.x
-    y = event.y
+del font , Combobox , Button , Entry , Frame , Label , Tk
 
 def debug( text ) :
     if is_debug :
@@ -133,39 +112,39 @@ def join( list ) :
             ret.append(i)
     return ret
 
-def load() :
-    path = filedialog.askopenfilename( initialdir = "./" )
+def file_load() :
+    path = askopenfilename( initialdir = ".\\save" )
     if not path :
         return
     debug(path)
     try :
         with open( path , encoding = "utf-8" ) as file :
-            data = json.load( file )
+            data = load( file )
         start , end = data["pos"]
         start_text_init( start )
         end_text_init( end )
         for i in range(3) :
-            num = list( forge_nums.keys() ).index( data["end"][i] )
+            num = list( forge.keys() ).index( data["end"][i] )
             end_combobox[i].current( num )
         output()
     except Exception as e :
-        messagebox.showerror( lang.get("error") , e )
+        showerror( lang.get("error") , e )
 
-def save() :
-    path = filedialog.asksaveasfilename( initialdir = "./" , initialfile = "data" , filetypes = [ [ "JSON" , ".json" ] ] )
+def file_save() :
+    path = asksaveasfilename( initialdir = ".\\save" , initialfile = "data" , filetypes = [ [ "JSON" , ".json" ] ] )
     if not path :
         return
     path += ".json"
     debug(path)
     data = {}
     data["pos"] = [ start_text.get() , end_text.get() ]
-    data["end"] = [ forge_name[ i.get() ] for i in end_combobox ]
+    data["end"] = [ forge_name()[ i.get() ] for i in end_combobox ]
     debug(data)
     try :
         with open( path , "w" , encoding = "utf-8" ) as file :
-            file.write( json.dumps( data , indent = 4 , separators = ( " ," , ": " ) , ensure_ascii = False ) )
+            file.write( dumps( data , indent = 4 , separators = ( " ," , ": " ) , ensure_ascii = False ) )
     except Exception as e :
-        messagebox.showerror( lang.get("error") , e )
+        showerror( lang.get("error") , e )
 
 def output() :
     output_text( cls = True )
@@ -185,8 +164,8 @@ def output() :
             end_text_init()
     num = end
     for i in end_combobox :
-        num -= forge_nums[ forge_name[ i.get() ] ]
-    l = list( forge_nums.values() )
+        num -= forge[ forge_name()[ i.get() ] ]
+    l = list( forge.values() )
     l.sort()
     I = start
     ret = []
@@ -201,19 +180,19 @@ def output() :
     for i in add :
         while ( I + i <= num ) and ( I + i <= 150 ) :
             I += i
-            ret.append( [ forge_nums_name[i] , 1 ] )
+            ret.append( [ forge_nums()[i] , 1 ] )
     for i in sub :
         while ( I + i >= num ) and ( I + i >= 0 ) :
             I += i
-            ret.append( [ forge_nums_name[i] , 1 ] )
-    while I < num :
-        I += 1
-        ret.append( [ "forge.punch" , 2 ] )
+            ret.append( [ forge_nums()[i] , 1 ] )
+    while I != num :
         ret.append( [ "forge.hit_light" , 1 ] )
-    while I > num :
-        I -= 1
-        ret.append( [ "forge.punch" , 1 ] )
-        ret.append( [ "forge.hit_light" , 1 ] )
+        if I < num :
+            I += 1
+            ret.append( [ "forge.punch" , 2 ] )
+        else :
+            I -= 1
+            ret.append( [ "forge.punch" , 1 ] )
     debug( f"{I}|{num}|{end}" )
     debug( [ i[0] for i in ret ] )
     if num <= 0 or num >= 150 :
@@ -231,8 +210,6 @@ def output() :
         output_text( f"{i[0]} * {i[1]}" )
 
 output_button.config( command = output )
-load_button.config( command = load )
-save_button.config( command = save )
-root.bind( "<B3-Motion>" , move )
-root.bind( "<Button-3>" , get_point )
+load_button.config( command = file_load )
+save_button.config( command = file_save )
 root.mainloop()
