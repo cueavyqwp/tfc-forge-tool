bb45ff698b2c186def7519000d8720af0ed3ceb5
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2024-02-13 15:17:28 +0800

    fix font bug and others

--- tfc_anvil.py
+++ tfc_anvil.py
@@ -2,7 +2,9 @@ import tkinter.scrolledtext
 import tkinter.messagebox
 import tkinter.filedialog
 import tkinter.ttk
+import traceback
 import tkinter
+import typing
 import json
 import os
 
@@ -32,18 +34,19 @@ forge = {
 
 class main :
 
-    def __init__( self , forge : dict[ str , int ] , font : list = [ "Consolas" , 18 , "bold" ] , color : bool = True , split : bool = True ) -> None :
+    def __init__( self , forge : dict[ str , int ] , font : list = [ "Microsoft YaHei" , 16 , "bold" ] , color : bool = True , split : bool = True ) -> None :
         self.config = { "color" : color , "split" : [ [] , [ "" ] ][ split ] }
         self.lang = langful.lang()
         self.root = tkinter.Tk()
+        if os.name == "nt" : self.root.attributes( "-topmost" , True )
         self.root.protocol( "WM_DELETE_WINDOW" , self.exit )
         self.root.title( self.lang[ "title" ] )
         self.root.geometry( f"400x700" )
-        [ self.root.attributes( *args ) for args in [ [ "-topmost" , True ] , [ "-transparent" ] ] ] if os.name == "nt" else None
         self.forge = forge
         self.font = font
 
     def exit( self , code = None ) -> None :
+        if code is None : code = 0
         self.root.destroy()
         exit( code )
 
@@ -52,17 +55,17 @@ class main :
         try :
             self.root.mainloop()
         except KeyboardInterrupt :
-            self.exit()
+            self.exit( 0 )
+        traceback.print_exc()
+        self.exit( 1 )
 
     @property
     def pos( self ) -> list :
         ret = []
         for entry in self.entry :
-            try :
-                i = int( entry.get() )
-                assert not ( i < 0 or i > 150 )
+            if ( text := entry.get() ).isalnum() and ( i := int( text ) ) > 0 and i < 150 :
                 ret.append( i )
-            except :
+            else :
                 ret.append( 0 )
                 entry.delete( 0 , "end" )
                 entry.insert( 0 , "0" )
@@ -77,39 +80,47 @@ class main :
                 ret.append( i )
         return [ f"{ name } * { num }" for name , num in ret ]
 
+    def trydo( self , func : typing.Callable ) -> None :
+        try : func( self )
+        except : tkinter.messagebox.showerror( self.lang[ "error" ] , traceback.format_exc() )
+
     def load( self , *args ) -> None :
         path = tkinter.filedialog.askopenfilename( initialdir = self.save_path )
         if not path : return
-        try :
+        def func( self : main ) -> None :
             with open( path , encoding = "utf-8" ) as file : data = json.load( file )
             entry = self.entry[ -1 ]
             entry.delete( 0 , "end" )
             entry.insert( 0 , str( data[ "pos" ] ) )
             [ self.combobox[ i ].current( list( self.forge.keys() ).index( data[ "end" ][ i ] ) ) for i in range( len( self.combobox ) ) ]
             self.output()
-        except Exception as e :
-            tkinter.messagebox.showerror( self.lang[ "error" ] , str( e ) )
+        self.trydo( func )
 
     def save( self , *args ) -> None :
         path = tkinter.filedialog.asksaveasfilename( initialdir = self.save_path , initialfile = "" , filetypes = [ ( "JSON" , ".json" ) ] )
         if not path : return
         if ( len( path ) < 5 ) or ( ".json" not in path ) : path += ".json"
         data = { "pos" : self.pos[ -1 ] , "end" : [ self.forge_name[ combobox.get() ] for combobox in self.combobox ] }
-        try :
+        def func( self : main ) -> None :
             with open( path , "w" , encoding = "utf-8" ) as file : json.dump( data , file , indent = 4 , separators = ( " ," , ": " ) , ensure_ascii = False )
-        except Exception as e :
-            tkinter.messagebox.showerror( self.lang[ "error" ] , str( e ) )
+        self.trydo( func )
+
+    def info_edit( self , func : typing.Callable ) -> None :
+        self.info.config( state = "normal" )
+        func( self )
+        self.info.config( state = "disabled" )
 
     def cls( self , *args ) -> None :
-        self.info.config( state = "normal" )
         self.info.delete( 0.0 , "end" )
-        self.info.config( state = "disabled" )
+
+    def clear( self , *args ) -> None :
+        self.info_edit( self.cls )
 
     def print( self , text : str = "" , cls : bool = False ) -> None :
-        self.cls() if cls else None
-        self.info.config( state = "normal" )
-        self.info.insert( "end" , str( text ) )
-        self.info.config( state = "disabled" )
+        def func( self : main ) -> None :
+            if cls : self.cls()
+            self.info.insert( "end" , str( text ) )
+        self.info_edit( func )
 
     def color( self , texts : list[ str ] ) -> None :
         for text , i in zip( texts , [ f"{ i + 1 }." for i in range( len( texts ) ) ] ) :
@@ -119,7 +130,7 @@ class main :
         [ self.info.tag_config( name , foreground = color ) for name , color in [ [ "forge" , "purple" ] , [ "num" , "yellowgreen" ] ] ]
 
     def output( self , *args ) -> None :
-        self.cls()
+        self.clear()
         num , end = self.pos
         l = list( self.forge.values() )
         ret = []
@@ -142,7 +153,7 @@ class main :
             end_forge = self.join( list( reversed( [ [ i.get() , 1 ] for i in self.combobox ] ) ) )
             ret = self.join( [ [ self.lang[ i[ 0 ] ] , i[ 1 ] ] for i in ret ] ) if ret else []
             self.print( s := "\n".join( [ i for i in [ [] , ret + self.config[ "split" ] ][ bool( ret ) ] + end_forge ] ) )
-            self.color( s.splitlines() ) if self.config[ "color" ] else None
+            if self.config[ "color" ] : self.color( str( s ).splitlines() )
 
     def init( self ) -> None :
         # values
@@ -156,7 +167,7 @@ class main :
         option_frame = tkinter.Frame( top_frame )
         combobox_frame = tkinter.Frame( option_frame )
         # pos entry
-        self.entry = []
+        self.entry : list[ tkinter.ttk.Entry ] = []
         for name in [ "pos.start" , "pos.end" ] :
             frame = tkinter.Frame( option_frame )
             tkinter.Label( frame , text = self.lang[ name ] ).pack( side = "left" , fill = "x" )
@@ -167,26 +178,26 @@ class main :
         self.pos
         # combobox
         tkinter.Label( combobox_frame , text = self.lang[ "end" ] ).pack( side = "left" )
-        self.combobox = []
+        self.combobox : list[ tkinter.ttk.Combobox ] = []
         for _ in range( 3 ) :
             combobox = tkinter.ttk.Combobox( combobox_frame , values = list( self.forge_name.keys() ) , state = "readonly" )
             combobox.current( 0 )
             combobox.pack( side = "top" , expand = True , fill = "x" , padx = 1 , pady = 1 , ipady = 5 )
             self.combobox.append( combobox )
+        # output button
+        tkinter.ttk.Button( top_frame , text = self.lang[ "output" ] , command = self.output ).pack( side = "right" , expand = True , fill = "both" , padx = 1 , pady = 1 )
         # pack frames
         top_frame.pack( side = "top" , fill = "x" )
         option_frame.pack( side = "left" )
         combobox_frame.pack( side = "top" , expand = True , fill = "x" )
         # load & save button
         [ tkinter.ttk.Button( option_frame , text = self.lang[ text ] , command = func ).pack( side = "top" , expand = True , fill = "both" , padx = 1 , pady = 1 ) for text , func in [ [ "save" , self.save ] , [ "load" , self.load ] ] ]
-        # output button
-        tkinter.ttk.Button( top_frame , text = self.lang[ "output" ] , command = self.output ).pack( side = "right" , expand = True , fill = "both" , padx = 1 , pady = 1 )
         # info text
         self.info = tkinter.scrolledtext.ScrolledText( self.root , relief = "ridge" , font = self.font )
         self.info.pack( side = "bottom" , expand = True , fill = "both" )
-        self.cls()
+        self.clear()
         # key bindings
-        [ self.root.bind( f"<{ key }>" , func ) for key , func in [ [ "Return" , self.output ] , [ "Control-l" , self.load ] , [ "Control-s" , self.save ] , [ "Delete" , self.cls ] ] ]
+        [ self.root.bind( f"<{ key }>" , func ) for key , func in [ [ "Return" , self.output ] , [ "Control-l" , self.load ] , [ "Control-s" , self.save ] , [ "Delete" , self.clear ] ] ]
 
 if __name__ == "__main__" :
     root = main( forge )
